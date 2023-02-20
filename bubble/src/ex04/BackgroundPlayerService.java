d8fe8271f36044c1d82d228bfc82a9bea7cbe8fe
HyooRin <khl4459@naver.com>
2023-02-20 16:14:13 +0900

    바닥오류 처리

--- bubble/src/ex04/BackgroundPlayerService.java
+++ bubble/src/ex04/BackgroundPlayerService.java
@@ -1,4 +1,4 @@
-package ex03;
+package ex04;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
@@ -32,9 +32,33 @@ public class BackgroundPlayerService implements Runnable {
 			// 오른쪽으로 갈 때는 역시나 기준 좌표지점을 보정 해야한다.
 
 			// 기준 왼쪽
-			Color leftColor = new Color(image.getRGB(player.getX() + 10, player.getY()));
+			Color leftColor = new Color(image.getRGB(player.getX() + 10, player.getY() + 25));
 			// 기준 오른쪽
-			Color rightColor = new Color(image.getRGB(player.getX() + 60, player.getY()));
+			Color rightColor = new Color(image.getRGB(player.getX() + 60, player.getY() + 25));
+			
+			
+			//바닥충돌감지기능 
+			//Color bottomColorLeft = new Color(image.getRGB(player.getX() + 10 , player.getY() + 60 ));
+			// -65536, -16776961, -1
+			
+			int bottomColorLeft = image.getRGB(player.getX() + 10 , player.getY() + 55 );
+			// -65535, -1677696, -1
+			int bottomColorRight = image.getRGB(player.getX() + 45, player.getX() + 55 );
+			// 하얀색이 아니면 바닥이다
+			if(bottomColorLeft + bottomColorRight != -2){
+				System.out.println("여기는 바닥입니다.");
+				player.setDown(false);
+			}else {
+				// 쪼금 점프하는 순간 bottomColorLeft -> -1 이 된다 
+				// 위 화살표를 누르면 현재 65번 반복 돌면서 y 값을 마이너스 해서
+				// 이미지를 위로 올리고 있고 현재 y 좌표에서 +130 좌표 올리고 --> down() 메서드 호출
+				// 올라가는 동시에 위 + 위 + 위 
+				if(player.isUp() == false && player.isDown() == false) {
+					// 다운 메서드가 한번 호출 되어야 한다 
+					player.down();
+				}
+			}
+			
 			
 			if( rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
 				player.setRight(false);
